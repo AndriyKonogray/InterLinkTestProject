66180a9140f340c4f07b648e039ea8f380cc2ef8
Andrew Kon <andriykonogray@gmail.com>
2019-01-23 22:05:52 +0200

    small update

--- src/main/Application.java
+++ src/main/Application.java
@@ -8,10 +8,10 @@ public class Application {
     public static void main(String[] args) {
         University university = new University("CH.U.I.");
         university.addStudent(new Student("Andrew","Kostenko", "MALE", 60));
-        university.addStudent(new Student("Julia", "Veselkina", "FEMALE", 89));
+        university.addStudent(new Student("Julia", "Veselkina", "FEMALE", 95));
         university.addStudent(new Student("Maria", "Perechrest", "FEMALE", 65));
-
-        Internship internship = new Internship("Interlink");
+        university.addStudent(new Student("Andrew", "Maslenko", "MALE", 91));
+        Internship internship = new Internship("Interlink", university);
         System.out.println("List of internship's students:");
         System.out.println(internship.getStudents());
     }
